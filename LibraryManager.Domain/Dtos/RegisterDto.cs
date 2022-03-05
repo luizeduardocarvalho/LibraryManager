ccabc040a5dbdc362784600ae86b8c8f7cbb42fb
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-03-05 16:31:05 +0000

    Add unique index for Email on Teacher
    Add the index to prevent creating two teachers with the same email

--- LibraryManager.Domain/Dtos/RegisterDto.cs
+++ LibraryManager.Domain/Dtos/RegisterDto.cs
@@ -8,7 +8,6 @@ namespace LibraryManager.Domain.Dtos
         public string Email { get; set; }
         [Required]
         public string Name { get; set; }
-        [Required]
         public string Password { get; set; }
         [Required]
         public string Role { get; set; }
---
+++ LibraryManager.Infrastructure/Migrations/20220305161301_CreateEmailIndexOnTeacher.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryManager.Infrastructure.Migrations
+{
+    public partial class CreateEmailIndexOnTeacher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Transaction",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 63, DateTimeKind.Unspecified).AddTicks(7330), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 465, DateTimeKind.Unspecified).AddTicks(6406), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Teacher",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 59, DateTimeKind.Unspecified).AddTicks(2110), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 464, DateTimeKind.Unspecified).AddTicks(447), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Student",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 57, DateTimeKind.Unspecified).AddTicks(2119), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 462, DateTimeKind.Unspecified).AddTicks(3342), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Book",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 54, DateTimeKind.Unspecified).AddTicks(1339), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 458, DateTimeKind.Unspecified).AddTicks(4986), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Author",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 42, DateTimeKind.Unspecified).AddTicks(2915), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 446, DateTimeKind.Unspecified).AddTicks(6188), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teacher_Email",
+                table: "Teacher",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Teacher_Email",
+                table: "Teacher");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Transaction",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 465, DateTimeKind.Unspecified).AddTicks(6406), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 63, DateTimeKind.Unspecified).AddTicks(7330), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Teacher",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 464, DateTimeKind.Unspecified).AddTicks(447), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 59, DateTimeKind.Unspecified).AddTicks(2110), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Student",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 462, DateTimeKind.Unspecified).AddTicks(3342), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 57, DateTimeKind.Unspecified).AddTicks(2119), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Book",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 458, DateTimeKind.Unspecified).AddTicks(4986), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 54, DateTimeKind.Unspecified).AddTicks(1339), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Author",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2022, 3, 1, 12, 4, 21, 446, DateTimeKind.Unspecified).AddTicks(6188), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTimeOffset(new DateTime(2022, 3, 5, 16, 13, 1, 42, DateTimeKind.Unspecified).AddTicks(2915), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
