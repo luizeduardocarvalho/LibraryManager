9e3a652ae1fb2d3bfc4cf1c57950c2d523d6cf7b
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-08-13 23:01:32 +0100

    Add GlobalUsings and update packages to their last version

--- LibraryManager.UnitTests/Controllers/BooksControllerTests.cs
+++ LibraryManager.UnitTests/Controllers/BooksControllerTests.cs
@@ -1,59 +1,48 @@
-﻿using FluentAssertions;
-using LibraryManager.Api.Controllers;
-using LibraryManager.Domain.Abstractions.Services;
-using LibraryManager.Domain.Entities;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NSubstitute;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿namespace LibraryManager.UnitTests.Controllers;
 
-namespace LibraryManager.UnitTests.Controllers
+[TestClass]
+public class BooksControllerTests
 {
-    [TestClass]
-    public class BooksControllerTests
-    {
-        private IBookService bookService;
-        private BooksController controller;
-        private Author author;
+    private IBookService bookService;
+    private BooksController controller;
+    private Author author;
 
-        [TestInitialize]
-        public void Initialize()
+    [TestInitialize]
+    public void Initialize()
+    {
+        bookService = Substitute.For<IBookService>();
+        controller = new BooksController(bookService);
+        author = new Author
         {
-            bookService = Substitute.For<IBookService>();
-            controller = new BooksController(bookService);
-            author = new Author
+            Id = 1,
+            Name = "Author"
+        };
+    }
+
+    [TestMethod]
+    public async Task GetAllShouldReturnOk()
+    {
+        // Arrange
+        var books = new List<Book>
+        {
+            new Book
             {
                 Id = 1,
-                Name = "Author"
-            };
-        }
+                Title = "Book Title",
+                Author = this.author,
+                Description = "Book Description",
+                Reference = 1,
+                CheckoutPeriod = 7
+            }
+        };
 
-        [TestMethod]
-        public async Task GetAllShouldReturnOk()
-        {
-            // Arrange
-            var books = new List<Book>
-            {
-                new Book
-                {
-                    Id = 1,
-                    Title = "Book Title",
-                    Author = this.author,
-                    Description = "Book Description",
-                    Reference = 1,
-                    CheckoutPeriod = 7
-                }
-            };
+        bookService.GetAll().Returns(books);
 
-            bookService.GetAll().Returns(books);
+        // Act
+        var expectedBooks = await controller.GetAll() as OkObjectResult;
 
-            // Act
-            var expectedBooks = await controller.GetAll() as OkObjectResult;
-
-            // Assert
-            expectedBooks.StatusCode.Should().Be(200);
-            expectedBooks.Value.Should().Be(books);
-        }
+        // Assert
+        expectedBooks.StatusCode.Should().Be(200);
+        expectedBooks.Value.Should().Be(books);
     }
 }
