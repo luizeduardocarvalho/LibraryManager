a69535154983f11ea0bf9bd2aff62dfc83bfeec8
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2023-04-16 17:27:46 +0100

    Add error details for logs

--- LibraryManager.Api/Startup.cs
+++ LibraryManager.Api/Startup.cs
@@ -147,7 +147,7 @@ public class Startup
         var user = uri.UserInfo.Split(':')[0];
         var passwd = uri.UserInfo.Split(':')[1];
         var port = uri.Port > 0 ? uri.Port : 5432;
-        var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4};SSL Mode=Require;Trust Server Certificate=True;",
+        var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4};SSL Mode=Require;Trust Server Certificate=True;Include Error Detail=True;",
             uri.Host, db, user, passwd, port);
         return connStr;
     }
